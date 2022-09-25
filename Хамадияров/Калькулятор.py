239023bbd1254e4a5acb9160899a68d9c01da06f
BillyRuslan <114331688+BillyRuslan@users.noreply.github.com>
2022-09-25 11:00:26 +0300

    Add files via upload

---
+++ Хамадияров/Калькулятор.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ведитее первое число: "))
+num2 = int(input("Ведитее второе число: "))
+
+print("Результат: ", num1 + num2)
+print("Результат: ", num1 - num2)
+print("Результат: ", num1 / num2)
+print("Результат: ", num1 * num2)
